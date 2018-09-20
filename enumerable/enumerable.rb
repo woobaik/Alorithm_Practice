4976ba3b642225be5b6d607100ea3c70426dc636
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-09-19 23:23:46 -0700

    finish my_select

--- enumerable/enumerable.rb
+++ enumerable/enumerable.rb
@@ -19,6 +19,13 @@ module Enumerable
   end
 
 
+  def my_select
+    result = []
+    self.my_each do |el|
+      result << el if yield(el)
+    end
+    result
+  end
 
 
 
@@ -26,6 +33,7 @@ end
 
 
 arr = %w(a b c d e)
+arr2 = (1..10).to_a
 #test my_each
 
 arr.my_each do |x|
@@ -36,3 +44,6 @@ end
 arr.my_each_with_index do |x, i|
   puts "#{i} : #{x}"
 end
+
+# test my_select
+arr2.my_select {|x| x % 2 == 0}
