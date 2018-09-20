cdb969b2e67fb89c08e110b58e53a732d874aa9d
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-09-19 23:02:03 -0700

    Finish bubble sort by that takes a block

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -14,3 +14,24 @@ def bubble_sort(arr)
 end
 
 bubble_sort([1,4,2,5,3,1,66])
+
+
+def bubble_sort_by(arr)
+  swapped = true
+
+  while swapped
+    swapped = false
+    0.upto(arr.length-2) do |idx|
+      if yield(arr[idx], arr[idx+1]) > 0
+        arr[idx], arr[idx+1] = arr[idx+1], arr[idx]
+        swapped = true
+      end
+    end
+  end
+  arr
+end
+
+
+bubble_sort_by(["a","hi","Chickafila","hello","hey"]) do |left,right|
+  left.length - right.length
+end
