cb2ab197cf3329796b9d14965e56cd7339d85589
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-09-26 00:57:07 -0700

    Finish palidrome with recursion

--- recursion/factorial.rb
+++ recursion/factorial.rb
@@ -3,4 +3,13 @@ def factorial(num)
   num * factorial(num -1)
 end
 
-factorial(10)
+# factorial(10)
+
+def palindrome(str)
+  return true if str.length == 1 || str.length == 0
+    if str[0] == str[-1]
+      palindrome(str[1..-2])
+    else
+      false
+    end
+end
