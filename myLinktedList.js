5636d8dd6abdafeb69b0155b5384d1683b8cc07c
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-04-21 17:31:18 -0700

    add reverse method

--- myLinktedList.js
+++ myLinktedList.js
@@ -1,4 +1,4 @@
-class myList {
+class LinkedList {
   constructor(value) {
     this.head = {
       value: value,
@@ -23,80 +23,106 @@ class myList {
       value: value,
       next: null
     }
-    newNode.next = this.head;
-    this.head = newNode;
+
+    newNode.next = this.head
+    this.head = newNode
     this.length += 1
   }
 
   print(value) {
-    const array = [];
+    const result = [];  
     let currentNode = this.head
-    while (currentNode !== null) {
-      array.push(currentNode.value)
+    while (currentNode) {
+      result.push(currentNode.value)
       currentNode = currentNode.next
     }
-    return array
+    return result
   }
 
   find(index) {
-    if (index > this.length || index < 0) {
-      return false
-    }
-    let counter = 0
+    if (index === 0) {
+      return this.head
+    } else if (index >= this.length) {
+      return -1
+    } else if (index < 0) {
+      return -1
+    } else {
+    let counter = 0;
     let currentNode = this.head
-    while (counter !== index) {
+
+    while(counter !== index) {
       currentNode = currentNode.next
       counter += 1
     }
     return currentNode
+    }
   }
-
+  
   insert(index, value) {
     if (index === 0 ) {
-     return this.prepend(value)
+      return this.prepend(value)
     }
+
     if (index > this.length) {
       return this.append(value)
     }
+    const prevNode = this.find(index - 1);
+    const nextNode = prevNode.next
     const newNode = {
       value: value, 
       next: null
-    }
-    const prevNode = this.find(index-1)
-    const nextNode = prevNode.next
-    prevNode.next = newNode
-    newNode.next = nextNode 
+    };
+    prevNode.next = newNode;
+    newNode.next = nextNode;
     this.length += 1
+    return this
   }
 
   remove(index) {
     if (index === 0) {
-      return this.head = this.head.next
+      this.head = this.head.next
+      this.length -= 1
+      return this
     }
-    if (index > this.length-1) {
-      return false
+
+    if (index >= this.length) {
+      return this
     }
-    const prevNode = this.find(index -1);
-    const target = prevNode.next;
-    const nextNode = target.next;
+    const lead = this.find(index-1);
+    const target = lead.next
+    const nextNode = target.next
+    lead.next = nextNode
+  }
 
-    prevNode.next = nextNode
-    this.length -= 1
-
+  reverse() {
+    if (this.length < 2) {
+      return this
+    }
+    let first = this.head;
+    this.tale = this.head
+    let second = first.next;
+    
+    while (second) {
+      let temp = second.next
+      second.next = first
+      first = second
+      second = temp
+    }
+    this.head.next = null;
+    this.head = first
+    
+    
+    return this
   }
 }
 
-const li = new myList(2);
-li.append(3);
-li.append(4);
+const li = new LinkedList(2)
+li.append(3)
 li.prepend(1)
-li.insert(0,0)
 li.insert(0,-1)
-li.insert(1,0.5)
-li.insert(9900,9)
-li.remove(1)
-li.remove(1)
+li.insert(1,1)
+li.insert(2,99)
+li.remove(0)
+li.remove(4)
+li.reverse()
 li.print()
-
-
-// refactor => make a newNode class.
