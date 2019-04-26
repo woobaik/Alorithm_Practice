f1ec384106fe001077237b13c8d78eb283e1fd78
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-04-25 17:03:53 -0700

    Finish bfs

---
+++ bi_search_bfs.js
@@ -0,0 +1,75 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearch {
+    constructor(value) {
+        this.root = null;
+    }
+
+    insert(value) {
+        if (!(this.root)) {
+            this.root = new Node(value);
+            return this
+        }
+        let currentNode = this.root;
+
+        while (true) {
+            if (value < currentNode.value) {
+                if (currentNode.left) {
+                    currentNode = currentNode.left
+                } else {
+                    currentNode.left = new Node(value)
+                    return this;
+                }
+            } else {
+                if (currentNode.right) {
+                    currentNode = currentNode.right
+                } else {
+                    currentNode.right = new Node(value);
+                    return this
+                }
+            }
+        }
+    }
+
+    breathSearch() {
+        const list = [];
+        const queue = [];
+        let currentNode = this.root;
+        if (currentNode) {
+            queue.push(currentNode);
+        }
+
+        while (queue.length > 0) {
+            currentNode = queue.shift();
+            list.push(currentNode.value)
+            if (currentNode.left) {
+                queue.push(currentNode.left)
+            }
+            if (currentNode.right) {
+                queue.push(currentNode.right)
+            }
+        }
+        return list
+    }
+}
+
+const bi = new BinarySearch();
+bi.insert(9)
+bi.insert(4);
+bi.insert(6);
+bi.insert(20);
+bi.insert(170);
+bi.insert(15);
+bi.insert(1);
+
+bi.breathSearch();
+
+//   1
+// 2   3 
+//4 5 6 7
