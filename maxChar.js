9324ad85fefa7bd5ba8902dcc6591acacb1d2075
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-04-26 18:20:54 -0700

    Finish maxChar

---
+++ maxChar.js
@@ -0,0 +1,23 @@
+function maxChar(str) {
+    const result = {};
+    let max = 0
+    let maxKey = ''
+    for (let char of str) {
+        if (!(result[char])) {
+            result[char] = 1;
+        } else {
+            result[char] += 1
+        }
+    }
+
+    for (let key in result) {
+        if (max < result[key]) {
+            max = result[key];
+            maxKey = key
+        }
+    }
+    return maxKey
+
+}
+
+maxChar('11');
