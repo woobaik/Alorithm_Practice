6e47e56a0680bf36b74df3b234648e7482586183
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-05-22 23:54:50 -0700

    Inorder_binary_tree

---
+++ leetcode/inorder_binary_tree.rb
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @return {Integer[]}
+def inorder_traversal(root)
+    return [] unless root
+    result = []
+    
+    result += inorder_traversal(root.left)
+    result << root.val
+    result += inorder_traversal(root.right)
+    
+    result
+end
