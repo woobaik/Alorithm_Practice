98d8049d2757e1e0c7328f34c717e5a59389d4fb
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-05-29 23:53:45 -0700

    Finish stair

---
+++ hackerrank/staircase.rb
@@ -0,0 +1,19 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the staircase function below.
+def staircase(n)
+    str = ' ' * n
+    i = str.length - 1
+    while i > -1 
+        str[i] = '#'
+        puts str 
+        i -= 1 
+    end
+end
+
+n = gets.to_i
+
+staircase n
