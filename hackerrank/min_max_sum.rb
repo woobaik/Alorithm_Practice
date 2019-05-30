3d25de44d4d965d3672ddd82b7976268b7172a2c
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-05-30 00:01:01 -0700

    finish minimax

---
+++ hackerrank/min_max_sum.rb
@@ -0,0 +1,17 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr)
+    arr.sort!
+    small = arr[0...-1].sum
+    large = arr[1..-1].sum
+    puts "#{small} #{large}"
+
+end
+
+arr = gets.rstrip.split(' ').map(&:to_i)
+
+miniMaxSum arr
