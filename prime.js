1f7368e286c4a4a8569581fa4696668429ee8633
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-10-19 00:18:54 -0700

    Finish finding prime

---
+++ prime.js
@@ -0,0 +1,21 @@
+const prime = num => {
+  const numArray = new Array(20).fill(true)
+  const result = []
+  numArray[0] = false
+  numArray[1] = false
+
+  for (let i = 2; i < Math.sqrt(num); i++) {
+    for (let j = 2; i * j < num; j++) {
+      numArray[i * j] = false
+    }
+  }
+
+  for (let i = 0; i < numArray.length; i++) {
+    if (numArray[i]) {
+      result.push(i)
+    }
+  }
+  return result
+}
+
+prime(20)
