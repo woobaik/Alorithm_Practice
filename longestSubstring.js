2a84303f261e7f88ff3209e7d24255990729e6ac
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-10-20 23:07:26 -0700

    Start longest substring

---
+++ longestSubstring.js
@@ -0,0 +1,13 @@
+const str = "abcabcbbb"
+const longestSubstr = str => {
+  const hash = {}
+  let startIdx = 0
+  let maxLength = 0
+
+  for (let i = 0; i < str.length; i++) {
+    if (hash[str[i]] > startIdx) {
+    }
+  }
+}
+
+longestSubstr(str)
