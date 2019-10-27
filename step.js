32a9cae6ba279d436675ca4fc82f5ec93da0e0f4
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-10-26 18:53:50 -0700

    Finish step

---
+++ step.js
@@ -0,0 +1,15 @@
+function step(num) {
+  for (let row = 0; row < num; row++) {
+    let result = ""
+    for (let col = 0; col < num; col++) {
+      if (row >= col) {
+        result += "#"
+      } else {
+        result += " "
+      }
+    }
+    console.log(result)
+  }
+}
+
+step(4)
