c34b1ea78dd3f7c92b692f28cb39762d64a6bb7f
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-12-06 23:57:51 -0800

    Fix error

--- longestSubstr.js
+++ longestSubstr.js
@@ -4,15 +4,16 @@ const lngestSub = arr => {
   let windowStart = 0
 
   for (let i = 0; i < arr.length; i++) {
-    if (hashMap[arr[i] >= windowStart]) {
-      windowStart = hashMap[arr[i]] + 1
+    const endChar = arr[i]
+    if (hashMap[endChar] >= windowStart) {
+      windowStart = hashMap[endChar] + 1
     }
-    hashMap[arr[i]] = i
+    hashMap[endChar] = i
 
-    maxLength = Math.max(maxLength, i - windowStart + 1)
+    maxLength = i - windowStart + 1
   }
   return maxLength
 }
 
-const arr = ["a", "b", "c", "d", "e", "f"]
+const arr = ["a", "b", "a", "c", "d", "e", "f"]
 lngestSub(arr)
