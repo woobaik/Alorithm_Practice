ba707d9061c51ef9b7eb795dd1a90149315ffcc5
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-12-10 11:37:26 -0800

    Finish roman again

--- roman.js
+++ roman.js
@@ -1,32 +1,32 @@
-const roman = roman => {
-  const digit = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000
-  }
-  const arr = roman.split("")
-  let total = 0
-  for (let i = 0; i < arr.length; i++) {
-    let current = digit[arr[i]]
-    let next = digit[arr[i + 1]]
-
-    if (current < next) {
-      console.log(current, next)
-      total += next - current
-      i += 1
-    } else if (next === undefined) {
-      total += current
-    } else {
-      total += current + next
-      i += 1
+const roman = str => {
+    const digit = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
     }
-  }
 
-  return total
+    const arr = [...str]
+    let total = 0
+    for (let i = 0; i < arr.length; i++) {
+        let current = digit[arr[i]]
+        let next = digit[arr[i + 1]]
+
+
+        if (current < next) {
+            total += next - current
+            i += 1
+        } else if (current >= next) {
+            total += current
+        } else {
+            total += current
+        }
+    }
+
+    return total
 }
 
-roman("MCXV")
+roman("MCMXCIV")
