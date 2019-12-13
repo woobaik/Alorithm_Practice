a07247c5cb12543333c79441922d058862279701
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-12-12 22:24:44 -0800

    add test case

--- maxChar.js
+++ maxChar.js
@@ -1,21 +1,22 @@
 function maxChar(str) {
-    const result = {};
-    let max = 0
-    let maxKey = ''
-    for (let char of str) {
-        if (!(result[char])) {
-            result[char] = 1;
-        } else {
-            result[char] += 1
-        }
+  const result = {}
+  let max = 0
+  let maxKey = ""
+  for (let char of str) {
+    if (!result[char]) {
+      result[char] = 1
+    } else {
+      result[char] += 1
     }
+  }
 
-    for (let key in result) {
-        if (max < result[key]) {
-            max = result[key];
-            maxKey = key
-        }
+  for (let key in result) {
+    if (max < result[key]) {
+      max = result[key]
+      maxKey = key
     }
-    return maxKey
-
+  }
+  return maxKey
 }
+
+maxChar("11")
