c36f586c3d0d8a5768f97c0e73d18cb8c2829784
Joungwoo Baik <joungwoo.baik@gmail.com>
2020-01-02 23:06:37 -0800

    Finish queue from top

---
+++ queueFromTop.js
@@ -0,0 +1,25 @@
+const Stack = require("./stack")
+
+class Queue {
+  constructor() {
+    this.first = new Stack()
+    this.second = new Stack()
+  }
+
+  add(record) {
+    this.first.push(record)
+  }
+
+  remove() {
+    while (this.first.peek()) {
+      const record = this.first.pop()
+      this.second.push(record)
+    }
+
+    this.second.pop()
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop())
+    }
+  }
+}
